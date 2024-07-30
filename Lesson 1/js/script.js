42aeb05a079949deaa43446cbd74bcf6a4f62bb1
VSabadash <vitaliiseptember@gmail.com>
2024-07-30 19:40:56 +0300

    first JS lesson

---
+++ Lesson 1/js/script.js
@@ -0,0 +1,9 @@
+console.log("test" + " " + "concatanation") // concatanation
+var name = "VITALII"
+let name1 = "Alex"
+console.log("Hello " + name) // concatanation
+console.log(name1)
+
+
+// logic 
+var out_div = document.getElementById("out").innerHTML = "Hello"
