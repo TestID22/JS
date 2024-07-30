ae662524234d4b040f041c8e152ed95d10747821
VSabadash <vitaliiseptember@gmail.com>
2024-07-30 19:47:36 +0300

    querySelector/finding elements

--- Lesson 1/js/script.js
+++ Lesson 1/js/script.js
@@ -5,5 +5,16 @@ console.log("Hello " + name) // concatanation
 console.log(name1)
 
 
-// logic 
+// logic. Find Eleement - change ellement 
 var out_div = document.getElementById("out").innerHTML = "Hello"
+var out_div = document.getElementById("out").innerHTML = "<b>a bit bold Hello</b>"
+
+
+document.querySelector("h2.header").innerHTML = 'viaHeader'
+// query - find via CLASS 
+document.querySelector(".header").innerHTML = 'CSS Selector'
+
+//paragraph find via ID 
+document.querySelector('#text').innerHTML = "My own desciption"
+
+
