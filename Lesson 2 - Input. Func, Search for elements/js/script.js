eafdfc960495b9ae19bbd46c9b760747a885ac59
VSabadash <vitaliiseptember@gmail.com>
2024-08-01 11:05:28 +0300

    added randomiser, func, work with an array

--- Lesson 2 - Input. Func, Search for elements/js/script.js
+++ Lesson 2 - Input. Func, Search for elements/js/script.js
@@ -12,3 +12,24 @@ document.querySelector('#ok').onclick = () => {
     output.innerHTML = input.value
     input.value = ''
 }
+
+
+// shift title in the button
+document.querySelector('.shifter_btn').onclick = shift;
+let shift_btn = document.querySelector('.shifter_btn')
+const allign = ['center', 'left', 'right']
+const colours = ['cyan', 'green', 'yellow', 'blue', 'red']
+
+function shift() {
+    shift_btn.style.textAlign = getRandomElement(allign)
+    shift_btn.style.background = getRandomColor(colours)
+}
+
+function getRandomElement(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+function getRandomColor(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length)
+    return arr[randomIndex]
+}
